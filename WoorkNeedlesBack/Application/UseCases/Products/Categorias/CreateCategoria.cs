1d0282b0fb1d75e1036d69504574cfc4b2c52c39
AUXILIAR DE SISTEMAS 1 <Auxsistemas@Iumafis.local>
2026-05-19 14:17:21 -0500

    reduccion use cases

--- WoorkNeedlesBack/Application/UseCases/Products/Categorias/CreateCategoria.cs
+++ WoorkNeedlesBack/Application/UseCases/Products/Categorias/CreateCategoria.cs
@@ -18,4 +18,46 @@ namespace Application.UseCases.Products.Categorias
             await unitofwork.SaveAsync();
         }
     }
+
+    public class DeleteCategoria(IUnitOfWork unitofwork)
+    {
+        public async Task Execute(int id)
+        {
+            var categoria = await unitofwork.Categorias.GetByIdAsync(id)
+                ?? throw new KeyNotFoundException("Categoría no encontrada.");
+            unitofwork.Categorias.Delete(categoria);
+            await unitofwork.SaveAsync();
+        }
+    }
+
+    public class GetAllCategorias(IUnitOfWork unitofwork, IMapper mapper)
+    {
+        public async Task<IEnumerable<CategoriaProductoDto>> Execute()
+        {
+            var categorias = await unitofwork.Categorias.GetAllAsync();
+            return mapper.Map<IEnumerable<CategoriaProductoDto>>(categorias);
+        }
+    }
+
+    public class GetCategoriaById(IUnitOfWork unitofwork, IMapper mapper)
+    {
+        public async Task<CategoriaProductoDto> Execute(int id)
+        {
+            var categoria = await unitofwork.Categorias.GetByIdAsync(id)
+                ?? throw new KeyNotFoundException("Categoría no encontrada.");
+            return mapper.Map<CategoriaProductoDto>(categoria);
+        }
+    }
+
+    public class UpdateCategoria(IUnitOfWork unitofwork)
+    {
+        public async Task Execute(int id, UpdateCategoriaProductoDto categoriaProductodto)
+        {
+            var categoria = await unitofwork.Categorias.GetByIdAsync(id)
+                ?? throw new KeyNotFoundException("Categoría no encontrada.");
+            categoria.Actualizar(categoriaProductodto.Nombre, categoriaProductodto.Descripcion);
+            unitofwork.Categorias.Update(categoria);
+            await unitofwork.SaveAsync();
+        }
+    }
 }
