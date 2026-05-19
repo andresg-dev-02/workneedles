ce52e7d8b8a976119b8ac4d462bbaa0fb2b8d67d
andresg-dev-02 <andreszf2022@gmail.com>
2026-05-19 18:32:49 -0500

    crud colores y tallas de producto finalizados

--- WoorkNeedlesBack/WoorkNeedles/Controller/Productos/TallaController.cs
+++ WoorkNeedlesBack/WoorkNeedles/Controller/Productos/TallaController.cs
@@ -1,5 +1,5 @@
 using Application.DTOs.Productos;
-using Application.UseCases.Products.Colores;
+using Application.UseCases.Products.Tallas;
 using Domain.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,32 +16,55 @@ namespace WoorkNeedles.Controller.Productos
     ) : ControllerBase
     {
         [HttpGet]
-    public async Task<IActionResult> GetAll()
-    {
-        var tallas = await getAll.Execute();
-        return Ok(tallas);
-    }
-
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetById(int id)
-    {
-        try
+        public async Task<IActionResult> GetAll()
         {
-            var talla = await getById.Execute(id);
-            return Ok(talla);
+            var tallas = await getAll.Execute();
+            return Ok(tallas);
         }
-        catch (KeyNotFoundException ex) { return NotFound(ex.Message); }
-    }
 
-    [HttpPost]
-    public async Task<IActionResult> Create([FromBody] CreateTallaDto dto)
-    {
-        try
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
         {
-            await create.Execute(dto);
-            return Created();
+            try
+            {
+                var talla = await getById.Execute(id);
+                return Ok(talla);
+            }
+            catch (KeyNotFoundException ex) { return NotFound(ex.Message); }
         }
-        catch (DomainException ex) { return BadRequest(ex.Message); }
-    }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateTallaDto dto)
+        {
+            try
+            {
+                await create.Execute(dto);
+                return Created();
+            }
+            catch (DomainException ex) { return BadRequest(ex.Message); }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateTallaDto dto)
+        {
+            try
+            {
+                await update.Execute(id, dto);
+                return Ok(new { message = "Talla actualizada exitosamente." });
+            }
+            catch (KeyNotFoundException ex) { return NotFound(ex.Message); }
+            catch (DomainException ex) { return BadRequest(ex.Message); }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await delete.Execute(id);
+                return Ok(new { message = "Talla eliminada exitosamente." });
+            }
+            catch (KeyNotFoundException ex) { return NotFound(ex.Message); }
+        }
     }
 }
